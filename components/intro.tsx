62b4227e7a9b1e3ca0504326573afa3ff15ff589
Tayrika <rika13tay@gmail.com>
2025-01-12 16:34:42 +0900

    Change into

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,11 +48,11 @@ export default function Intro() {
         </div>
       </div>
       <motion.p 
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        Hi, I'm Rika, a full-stack developer based in Tokyo, Japan. My focus is React and Next.js. Let's build something great together!
+        Hello, I'm Rika, a full-stack developer based in Tokyo, Japan. My focus is React and Next.js. Let's build something great together!
       </motion.p>
 
       <motion.div 
