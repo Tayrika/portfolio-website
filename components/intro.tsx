e6291be4449d4ac37681dc68453f9dde7a4924b3
Tayrika <rika13tay@gmail.com>
2025-01-12 17:30:20 +0900

    Remove photo icon and emoji

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,49 +11,13 @@ export default function Intro() {
   return (
     // section is a semamtic html tag where every element inside is related to each other vs div
     <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image 
-                src="https://images.unsplash.com/photo-1520763185298-1b434c919102?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                alt="Tulip icon" 
-                width={192}
-                height={192}
-                quality={100}
-                priority={true}
-                className="h-24 w-24 rounded-full object-cover border-[0.3rem] border-white shadow-xl"
-              />
-          </motion.div>
-            
-          <motion.span 
-            className="absolute bottom-0 right-0 text-3xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋🏻
-          </motion.span>
-        </div>
-      </div>
-      <motion.p 
+      <motion.h1 
         className="mb-10 mt-4 px-4 text-2xl !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         Hello, I'm Rika, a full-stack developer based in Tokyo, Japan. My focus is React and Next.js. Let's build something great together!
-      </motion.p>
+      </motion.h1>
 
       <motion.div 
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
