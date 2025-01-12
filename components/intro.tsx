9a2163dc11a6476622e1bdc2f13d1803d59ea724
Tayrika <rika13tay@gmail.com>
2025-01-12 16:02:35 +0900

    Add contact buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import React from "react"
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -44,9 +47,37 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+      <motion.p 
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
         Hi, I'm Rika, a full-stack developer based in Tokyo, Japan. My focus is React and Next.js. Let's build something great together!
-      </p>
+      </motion.p>
+
+      <motion.div 
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link 
+        href="#contact"
+        className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/rika-miyata-4bab99243/" target="_blank">
+          <BsLinkedin />
+        </a>
+
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10" href="https://github.com/Tayrika" target="_blank">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
