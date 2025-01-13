8a7926db518b4929d8575bb5017fc484a565f8ab
Tayrika <rika13tay@gmail.com>
2025-01-13 13:40:15 +0900

    Add project section

---
+++ components/projects.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+import { motion } from "framer-motion";
+
+export default function Projects() {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))
+        }
+      </div>
+    </motion.section>
+  );
+}
