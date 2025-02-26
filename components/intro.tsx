54d552d82591490454fbb80b11f0a7a47cd6f435
Tayrika <rika13tay@gmail.com>
2025-02-26 19:45:04 +0900

    Comment out import link

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { motion } from "framer-motion";
-import Link from "next/link";
+// import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { FaGithubSquare } from "react-icons/fa";
 
